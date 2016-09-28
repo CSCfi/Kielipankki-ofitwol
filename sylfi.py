381a871a3fdc1d8390dac4b632736b8ddb56a75b
Kimmo Koskenniemi <koskenni@lakka.kapsi.fi>
2016-09-28 10:50:42 +0300

    small corrections

--- sylfi.py
+++ sylfi.py
@@ -1,6 +1,6 @@
 import re, sys
-V = "(a|e|i|o|u|y|ä|ö)"
-C = "(b|c|d|f|g|h|j|k|l|m|n|p|q|r|s|š|t|v|x|z|ž)"
+V = "[aeiouyäö]"
+C = "[bcdfghjklmnpqrsštvxzž]"
 VV = "(aa|ee|ii|oo|uu|yy|ää|öö)"
 Vi = "(ai|ei|oi|ui|yi|äi|öi)"
 V12 = "(ie|uo|yö)"
@@ -9,17 +9,24 @@ V21 = "(au|äy|ou|öy|eu|ey|iy)"
 Syl1 = "(" + C + "*(" + V + "|" + VV + "|" +  Vi + "|" + V12 + "|" + V21 + ")" + C + "*)"
 S1 = re.compile(Syl1)
 
-Nuk =  "(" + V + "|" + VV + "|" + Vi + "|" + V21 + ")"
-Syl2 = "(" + Syl1 + "(" + Nuk + C + "*" + "|" + V21 + "))"
+Nuk =  "((" + V + "|" + VV + "|" + Vi + ")" + C + "*" + ")"
+Syl2 = "(" + Syl1 + "(" + Nuk + "|" + V21 + "))"
 S2 = re.compile(Syl2)
 
-# print(Syl1)
+Syl3 = "(" + Syl2 + C + "?(" + Nuk + "|" + V21 + "))"
+S3 = re.compile(Syl3)
+
+print(Syl1)
+print(Syl2)
+print(Syl3)
 
 def syllables(w):
     if S1.fullmatch(w):
         return("monosyllabic")
     elif S2.fullmatch(w):
         return("bisyllabic")
+    elif S3.fullmatch(w):
+        return("trisyllabic")
     else: return("other")
 
 for l in sys.stdin:
