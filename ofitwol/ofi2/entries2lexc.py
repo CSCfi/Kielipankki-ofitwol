6c91a9c11e0335da6431a2a4244ba9c7a47f9a2c
Kimmo Koskenniemi <koskenni@gmail.com>
2020-12-09 20:53:17 +0200

    generalised entry2lexc.py to contain pattern names

--- ofitwol/ofi2/entries2lexc.py
+++ ofitwol/ofi2/entries2lexc.py
@@ -1,4 +1,27 @@
-import re, sys, argparse
+import re
+
+multichar_set = set()
+
+def collect_mphons(expr_str):
+    mch_lst = re.findall(r"\{[^}]+\}|\+[A-Z0-9]+", expr_str)
+    for mch in mch_lst:
+        multichar_set.add(mch)
+
+def generated_base(mphon, cont):
+    if cont in suffix_dict:
+        base_mphon = mphon + suffix_dict[cont]
+        base_lst = generate.generate(base_mphon)
+        if base_lst:
+            base_str = min(base_lst, key=len)
+        else:
+            base_str = "**" + base_mphon + "**"
+    else:
+        base_str = mphon
+    base_str = base_str.replace("Ø", "")
+    base_str = base_str.replace("§", "_")
+    return base_str
+
+import argparse
 
 argparser = argparse.ArgumentParser(
     "python3 entries2lexc.py",
@@ -12,13 +35,22 @@ argparser = argparse.ArgumentParser(
     (e.g. '+GEN')""")
 argparser.add_argument(
     "-n", "--lexname",
-    help="""Name of the lexicon to be made out of the input entries if not given
-    by a LEXICON line in the input file.""",
+    help="""Name of the lexicon to be made out of the input
+    entries if not given by a LEXICON line in the input file.""",
     default="")
 argparser.add_argument(
     "-c", "--continuation",
     help="A default name of the continuation lexicon if the entry has none",
     default="")
+argparser.add_argument(
+    "-p", "--pattern-names",
+    help="Add the pattern names of reg exp entries",
+    action="store_true")
+argparser.add_argument(
+    "-s", "--base-suffixes",
+    help="""Dict of mophophonemic suffixes for base form generation
+    of each continuation clas""",
+    default="base-suffixes.json")
 argparser.add_argument(
     "-r", "--rules",
     help="Rule FST for generating base forms, default is None",
@@ -26,7 +58,6 @@ argparser.add_argument(
 args = argparser.parse_args()
 lexicon_name = args.lexname
 
-mch_set = set()
 definition_lst = []
 entry_lst = []
 
@@ -35,102 +66,121 @@ if args.rules:
     import hfst
     generate.init(args.rules)
 
-line_lst = []
+if args.base_suffixes:
+    import json
+    suffix_file = open(args.base_suffixes, "r")
+    suffix_dict = json.load(suffix_file)
+else:
+    suffix_dict = {}
+
+import sys
+
+ent_pat = re.compile(
+    r"""
+    (?P<entry> \S +)
+    (\s+ (?P<cont> [/A-ZÅÄÖŠŽa-zšžåäö] \S +))?
+    (\s + (?P<weight> [0-9] +))?
+    (\s*;)?
+    """, re.X)
+rege_pat = re.compile(
+    r"""
+    < (?P<re_entry> [^>]+ ) > # reg exp pattern
+    \s*                       #
+    (?P<cont> \S+ )           # continuation lexicon name
+    (
+      \s+                     #
+      (?P<weight> \d+ )       # weight
+      \s+
+      (?P<name> [A-ZŠŽÅÄÖ]+ ) # name of the pattern
+    )?
+    """, re.X)
+defi_pat = re.compile(r"^\s*[A-ZÅÄÖa-zåäö][A-ZÅÄÖa-zåäö0-9]+\s*=")
+
+defi_line_lst = []
+rege_line_lst = []
+
 for line_nl in sys.stdin:
     line, exclam, comment = line_nl.partition("!")
     line = re.sub(r"\s+", " ", line)
     line = line.strip()
+    if not line:
+        continue
     if line.startswith("LEXICON"):
         lexicon_keyword, space, lexicon_name = line.partition(" ")
         if not lexicon_name:
-            exit("*** LEXICON NAME EMPTY")
-        continue
-    mch_lst =re.findall(r"\{[^}]+\}|\+[A-Z0-9]+", line)
-    for mch in mch_lst:
-        mch_set.add(mch)
-    #print("line:", line) ###
-    if "=" in line or line_lst:
-        line_lst.append(line)
-        #print("line_lst:", line_lst) ###
-        if line.endswith(";"):
-            line_str = " ".join(line_lst)
-            line_str = line_str.strip(" \t\n;")
-            definition_lst.append(line_str)
-            line_lst = []
-        continue
-    else:
-        line = line.strip(" \t\n;")
-        if not line:
-            continue
+            exit("*** LEXICON NAME MISSING")
         entry_lst.append(line)
+        continue
+    
+    #print("line:", line) ###
 
-if mch_set:
+    if defi_line_lst or " = " in line:
+        # definitions may consist of several lines
+        defi_line_lst.append(line)
+        if line.endswith(";"):
+            line_str = " ".join(defi_line_lst)
+            line_str = line_str.strip(" \t\n;")
+            collect_mphons(line_str)
+            line_str = re.sub(r"([{}])", r"%\1", line_str)
+            definition_lst.append(line_str)
+            defi_line_lst = []
+
+    elif rege_line_lst or line.startswith("<"):
+        rege_line_lst.append(line)
+        if line.endswith(";"):
+            line_str = " ".join(rege_line_lst)
+            line_str = line_str.strip(" \t\n;")
+            collect_mphons(line_str)
+            line_str = re.sub(r"([{}])", r"%\1", line_str)
+            rege_mat = m_regent = rege_pat.fullmatch(line_str)
+            r = rege_mat.group("re_entry").strip()
+            c = rege_mat.group("cont")
+            w = rege_mat.group("weight")
+            n = rege_mat.group("name")
+            n = "°" + n if n and args.pattern_names else ""
+            if n:
+                multichar_set.add(n)
+            if w and n:
+                entry_lst.append("< {} {}:0::{} > {} ;".format(r, n, w, c))
+            elif w:
+                entry_lst.append("< {} 0::{} > {} ;".format(r, w, c))
+            elif n:
+                entry_lst.append("< {} {}:0 > {} ;".format(r, n, c))
+            else:
+                entry_lst.append("< {} > {} ;".format(r, c))
+            rege_line_lst = []
+
+    else:
+        ent_mat = ent_pat.fullmatch(line)
+        if not ent_mat:
+            exit("!!!!!!!!!!!!!!!!!!" + line)
+        #### print(ent_mat.groups()) ####
+        e = ent_mat.group("entry").replace("_", "{§}")
+        collect_mphons(e)
+        c = ent_mat.group("cont")
+        if not c:
+            c = args.continuation
+            if ":" not in e and args.rules:
+                e = generated_base(e, c) + ":" + e
+        w = ent_mat.group("weight")
+        w = ' "weight {}"'.format(w) if w else ""
+        entry = "{} {}{} ;".format(e, c, w)
+        entry_lst.append(entry)
+
+
+if multichar_set:
     print("Multichar_Symbols")
-    mch_lst = sorted(list(mch_set))
+    mch_lst = sorted(list(multichar_set))
     print(" ".join(mch_lst))
 
 if definition_lst:
     print("Definitions")
-for line in definition_lst:
-    definition = re.sub(r"([{}])", r"%\1", line)
+for definition in definition_lst:
     print(definition, ";")
 
-base_suffixes = {
-    "/s": "",
-    "/s12": "",
-    "/s2": "",
-    "/s3": "",
-    "/a": "",
-    "/acomp": "m{pm}i",
-    "/r": "",
-    "/rcomp": "m{pm}i",
-    "/v": "{dlnrtØ}{aä}",
-    "/v012": "{dlnrtØ}{aä}",
-    "/v02": "{dlnrtØ}{aä}",
-    "/v3": "{dlnrtØ}{aä}",
-    "/p": "",
-    "/pc": "",
-    "/po": "",
-    "/ps": ""
-}
+if not entry_lst[0].startswith("LEXICON"):
+    print("LEXICON", args.lexname)
 
-if not lexicon_name:
-    exit("*** LEXICON NAME NOT GIVEN AT ALL!")
-print("LEXICON", lexicon_name)
 for line in entry_lst:
-    if line.startswith("<"):
-        line = re.sub(r"([{}])", r"%\1", line)
-        ent, rb, cont = line.rpartition(">")
-        entry = ent + rb
-        continuation = cont.strip() if cont else args.continuation
-        print(entry, continuation, ";")
-    else:
-        entry, sp, rest = line.partition(" ")
-        entry = entry.replace("_", "{§}")
-        # entry = entry.replace("+", "%;+", 1) ###
-        if rest:
-            cont, sp, wght = rest.partition(" ")
-            continuation = cont if cont else args.continuation
-            weight = ' "weight: ' + wght + '"' if wght else ""
-        else:
-            continuation = args.continuation
-            weight = ''
-        baseform, colon, mphon_form = entry.partition(":")
-        #print("entry, continuation, weight", entry, continuation, weight)
-        #print("baseform", baseform) ###
-        if args.rules and not colon:
-            if continuation in base_suffixes:
-                base_mphon = baseform + base_suffixes[continuation]
-                base_lst = generate.generate(base_mphon)
-                if base_lst:
-                    base_str = min(base_lst, key=len)
-                else:
-                    base_str = "**" + base_mphon + "**"
-            else:
-                base_str = baseform
-            base_str = base_str.replace("Ø", "")
-            entry = base_str + ":" + entry            
-
-        #print("entry, continuation, weight", entry, continuation, weight) ###
-        print(entry, continuation + weight, ";")
+    print(line)
 
